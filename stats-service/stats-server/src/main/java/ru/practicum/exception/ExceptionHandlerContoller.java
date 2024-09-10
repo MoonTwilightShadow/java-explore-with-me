217fe9e54eaa1ae0eaaca30b49f0ad2d11031177
alex <lazutin.leh@yandex.ru>
2024-09-10 15:32:39 +0300

    try 5th

--- stats-service/stats-server/src/main/java/ru/practicum/exception/ExceptionHandlerContoller.java
+++ stats-service/stats-server/src/main/java/ru/practicum/exception/ExceptionHandlerContoller.java
@@ -2,9 +2,12 @@ package ru.practicum.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class ExceptionHandlerContoller {
@@ -17,6 +20,12 @@ public class ExceptionHandlerContoller {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse availableHandle(final IllegalArgumentException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse missingParameterHandle(final MissingServletRequestParameterException e) {
         return new ErrorResponse(e.getMessage());
     }
 
